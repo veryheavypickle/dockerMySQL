dc0034d15b2263227894d06ddc0eaf8b517a5575
veryheavypickle <xavier.carroll01@gmail.com>
2022-09-02 19:06:28 +0200

    Added basic MySQL python script

---
+++ main.py
@@ -0,0 +1,49 @@
+import mysql.connector
+
+
+def main():
+    if not {"Database": dbName} in executeSQL("SHOW DATABASES", use=False):
+        createDB()
+
+
+def createDB():
+    executeSQL("CREATE DATABASE " + dbName, use=False)
+    executeSQL("""
+    CREATE TABLE IF NOT EXISTS customers (
+    id VARCHAR(15) PRIMARY KEY,
+    coolData VARCHAR(30) NOT NULL,) engine=InnoDB;
+    """)
+
+
+def executeSQL(command, parameters=(), use=True):
+    try:
+        database = mysql.connector.connect(**config)
+        database.get_warnings = True
+        cursor = database.cursor(dictionary=True)
+    except mysql.connector.errors.ProgrammingError as e:
+        print(e)
+        exit()
+    except mysql.connector.errors.DatabaseError as e:
+        print("FATAL - MySQL probably doesn't exist on this system - {0}".format(e))
+        exit()
+    try:
+        if use:
+            cursor.execute("USE {0}".format(dbName))
+        cursor.execute(command, parameters)
+    except mysql.connector.errors.ProgrammingError as e:
+        print("FATAL - Try dropping the whole database and retrying - {0}".format(e))
+        exit()
+    rows = cursor.fetchall()
+    database.commit()
+    database.close()
+    return rows
+
+
+if __name__ == '__main__':
+    config = {
+        "host": "localhost",
+        "user": "root",
+        "password": "root"
+    }
+    dbName = "testDB"
+    main()
